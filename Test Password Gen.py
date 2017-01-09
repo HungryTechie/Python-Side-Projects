a87d04930127fd9caf07d8abed7482b313df820d
Max <maximusr535@gmail.com>
2017-01-08 22:59:28 -0500

    Update Test Password Gen.py
    most passwords dont use "!@#$%^&*()"
    otherwise very solid

--- Test Password Gen.py
+++ Test Password Gen.py
@@ -2,7 +2,7 @@
 
 import random #imports the random.sample func
 
-c = "abcdefhijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+c = "abcdefhijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 passlen = int(input("How many characters would you like in your password?"))
 p = "".join(random.sample(c,passlen ))
 print(p)
